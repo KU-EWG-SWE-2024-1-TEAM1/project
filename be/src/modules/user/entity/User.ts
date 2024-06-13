d930a2cfd3cfeb00c58e7634060f9ecb46c4da12
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-06-13 09:50:55 +0900

    feat.password password salt 추가하고 bcrypt 해싱

--- be/src/modules/user/entity/User.ts
+++ be/src/modules/user/entity/User.ts
@@ -1,6 +1,7 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, BeforeInsert } from "typeorm";
 import { Post } from "../../post/entity/Post";
 import { Comment } from '../../comment/entity/Comment';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class User {
@@ -13,8 +14,19 @@ export class User {
     @Column()
     email: string;
 
-    @Column()
+    @Column({ default: '' })
     password: string;
+
+    @Column({ default: '' })
+    salt: string;
+
+    @BeforeInsert()
+    async hashPassword() {
+        if (this.password) {
+            this.salt = await bcrypt.genSalt();
+            this.password = await bcrypt.hash(this.password, this.salt);
+        }
+    }
 
     @OneToMany(() => Post, post => post.user)
     posts: Post[];
