0317803e6954e4d95bc0343a1daac71fc599763b
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-06-25 16:02:10 +0900

    fix.config 환경변수 수정

--- fe/next.config.js
+++ fe/next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
         AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
         AWS_REGION: process.env.AWS_REGION,
         S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
-        NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL
+        NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+        NEXT_PUBLIC_MOVIE_API_KEY: process.env.NEXT_PUBLIC_MOVIE_API_KEY
     },
     images: {
         remotePatterns: [
