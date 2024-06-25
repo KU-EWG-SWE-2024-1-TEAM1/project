add2c1b0a9f13a124e014bb75faa3bc66c69acb8
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-06-25 12:17:35 +0900

    fix.env 환경변수 공개범위 조절

--- fe/app/utils/aws.js
+++ fe/app/utils/aws.js
@@ -4,14 +4,14 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 const s3Client = new S3Client({
     region: "ap-northeast-2",
     credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
 });
 
 export const generatePresignedUrl = async (fileName, fileType) => {
     const params = {
-        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+        Bucket: process.env.S3_BUCKET_NAME,
         Key: fileName,
         ContentType: fileType,
     };
